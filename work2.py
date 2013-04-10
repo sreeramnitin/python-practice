69b734a99b07bd9493b877b27ad2890715c79ecd
Nitin s <s.nani100@gmail.com>
2013-04-10 22:01:08 +0530

    Update work2.py

--- work2.py
+++ work2.py
@@ -8,11 +8,12 @@ sum=2
 a1=1
 a2=2
 a3=0
-for i in range(1,10-2):
- a3=a1+a2
- a1=a2
- a2=a3
- if(a3%2==0):
-   sum=sum+a3
+while(a3 < 4000000000):
+    a3=a1+a2
+    a1=a2
+    a2=a3
+    if(a3%2==0):
+    sum=sum+a3
 print sum
+
 
